54cc8e93adc03e97942a86d29b29e87baf7b0c05
Saichaitanya Kanne <109675832+chaitanya013@users.noreply.github.com>
2022-12-03 15:50:59 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// if output is 0 odd else even
+
+int main(){
+    int x;
+    printf("enter number:");
+    scanf("%d",&x);
+    printf("%d \n",x %2==0);
+    return 0;
+
+}
+
+odd
+
+int main(){
+    int x;
+    printf("enter number:");
+    scanf("%d",&x);
+    printf("%d",x%2!=0);
+    return 0;
+}
